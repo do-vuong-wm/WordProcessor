0a8c391b608f54c1f27424a1aa7c10aee1bde18c
Vuong Do <vuongdotf2@gmail.com>
2019-11-19 23:33:13 -0700

    Added save and open feature. GUI now inherit JFrame instead.

--- WordProcessor/src/WordProcessorGui.java
+++ WordProcessor/src/WordProcessorGui.java
@@ -2,45 +2,57 @@
  * 
  */
 
-import java.awt.Color;
 import java.awt.*;  
+import java.awt.event.*;  
 import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  * @author Vuong
  *
  */
-public class WordProcessorGui {
+
+@SuppressWarnings("serial")
+public class WordProcessorGui extends JFrame implements ActionListener{
 	
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Word Processor");
+	private JMenuItem openFile, saveFile;
+	private JTextArea previewDisplay, errorLogDisplay;
+	private File processFile;
+	
+	WordProcessorGui(){
 		
 		GridBagLayout grid = new GridBagLayout();
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		
-		frame.setSize(500, 800);
-		frame.setMinimumSize(frame.getSize());
-		frame.setPreferredSize(frame.getSize());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(grid);
-		//frame.setResizable(false);
+		setTitle("Word Processor");
+		setSize(660, 800);
+		setMinimumSize(getSize());
+		setPreferredSize(getSize());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(grid);
+		setResizable(false);
 		
 		/*******************Menu********************/
 		
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Menu");
-		JMenuItem openFile, saveFile;
 		
 		openFile = new JMenuItem("Open File and Process");
 		saveFile = new JMenuItem("Save File");
+		
+		// Add listener
+		openFile.addActionListener(this);
+		saveFile.addActionListener(this);
 		
 		menu.add(openFile);
 		menu.add(saveFile);
 		
 		menuBar.add(menu);
 		
-		frame.setJMenuBar(menuBar);
+		setJMenuBar(menuBar);
 		
 		/*****************Panels*******************/
 //		JLabel preview, errorLog;
@@ -54,31 +66,89 @@ public class WordProcessorGui {
 		errorLogPanel.setBorder(BorderFactory.createTitledBorder("Error Log"));
 		
 		/******************Text Area********************/
+		Font font = new Font("Arial", Font.PLAIN, 12);
 		
-		JTextArea previewDisplay = new JTextArea(30, 40); 
-		previewDisplay.setEditable(false);
+		previewDisplay = new JTextArea(30, 51);
+		previewDisplay.setMargin(new Insets(10,10,10,10));
+		previewDisplay.setLineWrap(true);
+		previewDisplay.setFont(font);
+		previewDisplay.setForeground(Color.black);
+		//previewDisplay.setEditable(false);
 		JScrollPane previewScroll = new JScrollPane(previewDisplay);
 		
-		JTextArea errorLogDisplay = new JTextArea(10, 40); 
+		errorLogDisplay = new JTextArea("Error: An Error", 10, 51); 
+		errorLogDisplay.setMargin(new Insets(10,10,10,10));
+		errorLogDisplay.setLineWrap(true);
+		errorLogDisplay.setFont(font);
+		errorLogDisplay.setForeground(Color.red);
 		errorLogDisplay.setEditable(false);
 		JScrollPane errorLogScroll = new JScrollPane(errorLogDisplay);
 		
 		previewScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		previewPanel.add(previewScroll);
+		previewScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		previewPanel.add(previewScroll,gbc);
 		
 		errorLogScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		errorLogPanel.add(errorLogScroll);
+		errorLogScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		errorLogPanel.add(errorLogScroll,gbc);
 		
 		/***********************************************/
-
+		//gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
 	    gbc.gridx = 0;  
 	    gbc.gridy = 0;
-		frame.add(previewPanel, gbc);
+		add(previewPanel, gbc);
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 0.5;
 	    gbc.gridx = 0;
 	    gbc.gridy = 1;
-		frame.add(errorLogPanel, gbc);
+		add(errorLogPanel, gbc);
 		
-		frame.setVisible(true);
+		pack();
+		setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {    
+		if(e.getSource() == openFile) {
+		    JFileChooser fc = new JFileChooser();
+		    int i = fc.showOpenDialog(this);    
+		    if(i == JFileChooser.APPROVE_OPTION) { 
+		        processFile = fc.getSelectedFile(); // this is a File object   
+		        String filepath= processFile.getPath();
+		        // Call processing function in here on File object then display new File object
+		        try {
+		        BufferedReader br=new BufferedReader(new FileReader(filepath));    
+		        String s1="",s2="";                         
+		        while((s1=br.readLine()) != null) {
+		        s2+=s1+"\n";    
+		        }
+		        previewDisplay.setText(s2);    
+		        br.close();
+		        }catch(Exception ex) {
+		        	ex.printStackTrace();
+		        }                 
+		    } 
+		}else if(e.getSource() == saveFile) {
+		    JFileChooser fc = new JFileChooser();
+		    int i = fc.showSaveDialog(this);
+		    if(i == JFileChooser.APPROVE_OPTION) { 
+		    	try {
+		    		if(processFile != null)
+		    			Files.copy(processFile.toPath(), fc.getSelectedFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
+		    	}catch(Exception ex) {
+		        	ex.printStackTrace();
+		        }  
+		    }
+		}
+	}
+	
+	public static void main(String[] args) {
+		WordProcessorGui wpg = new WordProcessorGui();
 	}
 	
 }
